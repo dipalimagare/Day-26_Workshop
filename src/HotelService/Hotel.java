6245861e515ba4d9b4ae808833ea939a73235225
dipalimagare <dipalimagare2026@gmail.com>
2021-08-23 10:45:08 +0530

    Uploading Workshop file

--- src/HotelService/Hotel.java
+++ src/HotelService/Hotel.java
@@ -1,6 +1,8 @@
 
 package HotelService;
+import java.time.DayOfWeek;
 import HotelReservationCLI.*;
+
 public class Hotel {
 	public String Name;
 
@@ -48,4 +50,28 @@ public class Hotel {
 		this.WeekdayRate = WeekdayRate;
 		this.WeekendRate = WeekendRate;
 	}
-}
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation System");
+
+		Hotel lakewood = new Hotel();
+		lakewood.setName("Lakewood");
+		lakewood.setRating(110);
+		System.out.println("Hotel Name is" + lakewood.getName());
+		System.out.println("Rate for Regular customer is : " + lakewood.getRating());
+
+		Hotel bridgewood = new Hotel();
+		bridgewood.setName("Bridgewood");
+		bridgewood.setRating(160);
+		System.out.println("Hotel Name is" + bridgewood.getName());
+		System.out.println("Rate for Regular customer is :" + bridgewood.getRating());
+
+		Hotel ridgewood = new Hotel();
+		ridgewood.setName("Bridgewood");
+		ridgewood.setRating(220);
+		System.out.println("Hotel Name is" + ridgewood.getName());
+		System.out.println("Rate for Regular customer is : " + ridgewood.getRating());
+	}
+	
+	}
+
+	
